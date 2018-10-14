8229594db31a58c028125246c00e52b1d73d0b4f
jbmominee <jaymominee@gmail.com>
2018-10-14 10:45:36 -0500

    CP-03-JS-Variables ** learned about let, var, const

---
+++ Prog-with-JS/03-JS-Variables.js
@@ -0,0 +1,31 @@
+/*
+Javascript Variables
+
+What type of variable name casing do you use in JavaScript? (think of snakes
+or camels)
+camelCase where the first word is lowercase and each additional word
+begins with uppercase.
+
+If you want to create a variable called mentor and set its value equal to the
+name of your mentor, how would you do that?
+var mentor = ‘Alex Rolfe’;
+
+How would you then log the value of mentor to the console?
+console.log(mentor); will output ‘Alex Rolfe’
+
+Describe the difference between let, var, and const.
+A const is a variable that cannot be reassigned or is always constant.
+A let is a variable that can be reassigned a different value but is confined
+to predetermined block of code.
+Var can be reassigned and have a global scope.
+
+Code
+
+Using var, let and const create 3 variables that will hold your name, state,
+and hair color.
+
+*/
+
+var name = ‘Jay’;
+const hairColor = ‘Brown’;
+let state = ‘Wisconsin’;
