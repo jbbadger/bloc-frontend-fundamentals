3968984ca636b7851037cb0938414649aa4903c4
jbmominee <jaymominee@gmail.com>
2018-10-14 10:36:00 -0500

    CP-02-Primitive-Data **learned JS primitive data types

---
+++ Prog-with-JS/02-Prim-Data-Types.js
@@ -0,0 +1,24 @@
+/* Primitive Data Types
+
+What are JavaScript's five main primitive data types?
+String, number, Boolean, null, and undefined.
+
+What are the only two values available to the boolean data type?
+True and false.
+
+If you create something in JavaScript but don't set a value,
+what data type is it given?
+Undefined.
+
+If you wanted to convert a number to a string, how would you do
+that in JavaScript?
+toString()
+
+What built-in method would you use to determine the length of a
+given string?
+.length;
+
+Create a string that is equal to your name and use charAt to access 
+the first letter of your name.*/
+
+console.log('Jay'.charAt(0)); // outputs “J”
