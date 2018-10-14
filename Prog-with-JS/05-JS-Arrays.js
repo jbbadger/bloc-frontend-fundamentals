b9b3bb36cf9dd9a17af9de33063fff40efe03f3c
jbmominee <jaymominee@gmail.com>
2018-10-14 12:35:17 -0500

    CP-05-JS-Arrays **learned about arrays, pushed new item to an array

---
+++ Prog-with-JS/05-JS-Arrays.js
@@ -0,0 +1,28 @@
+/*
+Arrays
+
+Describe an array.
+An array is an indexed collection of items or more succinctly, a list.
+
+In JavaScript can you have a single array with a string and a number in it?
+Multiple data types can be stored within a single array but it’s not a
+recommended practice.
+
+In JavaScript do arrays start at index 0 or index 1?
+JavaScript uses zero-indexing.
+
+Code
+
+Using the clothes variable below use the push() method to add a new item to
+the clothes array. The new item should be called pants.
+*/
+
+var clothes = ['collared shirt', 'shirt'];
+
+// add code below
+
+clothes.push('pants')
+
+// add code above
+
+console.log(clothes);
